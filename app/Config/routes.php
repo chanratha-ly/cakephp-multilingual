6c14b48a514db151811b127bb4964c622425c019
chanratha-ly <chanratha.ly@gmail.com>
2013-06-20 12:06:36 +0700

    Change route

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -25,7 +25,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+Router::connect('/', array('controller' => 'menus', 'action' => 'index', 'eng'));
 /**
  * Load all plugin routes.  See the CakePlugin documentation on 
  * how to customize the loading of plugin routes.
